5470f56967de5c2efc2fde26e358af3e6574cd7c
Kevyn Lopes <kevynlopes2019@gmail.com>
2023-09-07 21:52:39 -0300

    Initial commit

---
+++ Acerte/Program.cs
@@ -0,0 +1,16 @@
+﻿void acerteNumero()
+{
+    Random aleatorio = new Random();
+    int numeroSecreto = aleatorio.Next(1, 101);
+    int numero;
+    Console.WriteLine(numeroSecreto);
+    do
+    {
+        Console.WriteLine("Digite um numero de 1 a 100");
+        numero = int.Parse(Console.ReadLine());
+        
+    } while (numero != numeroSecreto);
+    Console.WriteLine("Voce acertou!");
+}
+
+acerteNumero();
